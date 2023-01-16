8214a361938301bfa3c8617eb384ebd605c3a87b
Nick Fulghum <nickfulghum92@gmail.com>
2023-01-16 16:26:46 -0600

    created search form component

--- frontend/resolution/src/components/SearchForm.js
+++ frontend/resolution/src/components/SearchForm.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+    Paper,
+    InputBase,
+    IconButton,
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
-const SearchForm = () => {
+const SearchForm = ({ searchFor }) => {
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        searchFor(searchTerm.trim() || undefined);
+        setSearchTerm(searchTerm.trim());
+    }
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
     return (
-        <div>SearchForm</div>
+        <Paper
+            component="form"
+            sx={{
+                p: "2px 4px",
+                display: "flex",
+                justifyContent: "center",
+                width: 300,
+                my: 1,
+            }}
+            onSubmit={handleSubmit}
+        >
+            <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Find your dream job"
+                inputProps={{ "aria-label": "search-companies" }}
+                name="searchTerm"
+                value={searchTerm}
+                onChange={handleChange}
+            />
+            <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+                <SearchIcon />
+            </IconButton>
+        </Paper>
     )
 }
 
