ec30a5fc2c74d32b2c6ccc595d7b836464750369
Nick Fulghum <nickfulghum92@gmail.com>
2023-01-16 10:48:49 -0600

    added login form

--- frontend/resolution/src/components/Login.js
+++ frontend/resolution/src/components/Login.js
@@ -1,8 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+    Button,
+    CssBaseline,
+    TextField,
+    Link,
+    Grid,
+    Box,
+    Typography,
+    Container
+} from '@mui/material';
 
-const Login = () => {
+
+const Login = ({ login }) => {
+
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+    });
+    const [formErrors, setFormErrors] = useState([]);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        let result = await login(formData);
+        if (result.success) {
+            navigate('/');
+        } else {
+            setFormErrors(result.errors);
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(data => ({ ...data, [name]: value }));
+    }
+
     return (
-        <div>Login</div>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography component="h1" variant="h5" color='#bf360c'>
+                    Sign in
+                </Typography>
+                <form onSubmit={handleSubmit}>
+                    <Box noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Username"
+                            name="username"
+                            autoComplete="username"
+                            autoFocus
+                            value={formData.username}
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            style={{ background: '#bf360c' }}
+                        >
+                            Login
+                        </Button>
+                        <Grid item>
+                            <Link href="signup" variant="body2" color='#bf360c'>
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Box>
+                </form>
+            </Box>
+        </Container>
     )
 }
 
--- frontend/resolution/src/components/Navigation.js
+++ frontend/resolution/src/components/Navigation.js
@@ -20,22 +20,24 @@ const Navigation = () => {
 
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <Toolbar>
-                <Link {...linkProps} to='/'>
-                    <img src={smallLogo} alt="logo" style={{ width: '100px', height: '100px', margin: '0px 10px' }} />
-                </Link>
-                <Typography variant="h6" component="div" color='#bf360c' sx={{ flexGrow: 1 }}>
-                    You're Resolution Buddy 365 days a year!
-                </Typography>
-                <>
-                    <Link sx={{ mr: 2 }} {...linkProps} to="exercise">Exercise</Link>
-                    <Link sx={{ mr: 2 }} {...linkProps} to="nutrition">Nutrition</Link>
-                    <Link sx={{ mr: 2 }} {...linkProps} to="profile">Profile</Link>
-                    <Link sx={{ mr: 2 }} {...linkProps} to="logout">Logout</Link>
-                    <Link sx={{ mr: 2 }} {...linkProps} to="signup">Sign Up</Link>
-                    <Link sx={{ mr: 2 }} {...linkProps} to="login">Login</Link>
-                </>
-            </Toolbar>
+            <AppBar position="sticky" style={{ background: '#e0e0e0' }}>
+                <Toolbar>
+                    <Link {...linkProps} to='/'>
+                        <img src={smallLogo} alt="logo" style={{ width: '100px', height: '100px', margin: '0px 10px' }} />
+                    </Link>
+                    <Typography variant="h6" component="div" color='#bf360c' sx={{ flexGrow: 1 }}>
+                        RESOLUTION BUDDY
+                    </Typography>
+                    <>
+                        <Link sx={{ mr: 2 }} {...linkProps} to="exercise">Exercise</Link>
+                        <Link sx={{ mr: 2 }} {...linkProps} to="nutrition">Nutrition</Link>
+                        <Link sx={{ mr: 2 }} {...linkProps} to="profile">Profile</Link>
+                        <Link sx={{ mr: 2 }} {...linkProps} to="logout">Logout</Link>
+                        <Link sx={{ mr: 2 }} {...linkProps} to="signup">Sign Up</Link>
+                        <Link sx={{ mr: 2 }} {...linkProps} to="login">Login</Link>
+                    </>
+                </Toolbar>
+            </AppBar>
         </Box>
     )
 }
